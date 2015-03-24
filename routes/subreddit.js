a090cb33dc01bd792420f1887475b6d60ece134b
Chris Gregori <chrisgreg23@googlemail.com>
2015-03-24 22:39:10 +0000

    Added more comments, renamed some variables

--- routes/subreddit.js
+++ routes/subreddit.js
@@ -7,19 +7,25 @@ var links = [];
 
 // Middleware to retrieve subreddit information
 router.use(function (req, res, next) {
-  console.log('Building information for', req.body.subreddit);
-  next();
+	console.log('Building information for', req.body.subreddit);
+	next();
 });
 
 /* GET subreddit listing. - Redirect you to */
 router.get('/', function(req, res) {
-   res.redirect('/');
+	res.redirect('/');
 });
 
 // Middleware to extract images
 router.use(function (req, res, next) {
 
-	reddit.r(req.body.subreddit.replace('/r/','').replace('/', ''))[req.body.sort]().from(req.body.from).limit(100, function(err, data, res){	
+	var subreddit_name = req.body.subreddit.replace('/r/','').replace('/', '');
+	var sorting = req.body.sort;
+	var from_range = req.body.from;
+	var post_limit = 100;
+
+	// Grab links from subreddit, applying limit, range and sort type
+	reddit.r(subreddit_name)[sorting]().from(from_range).limit(post_limit, function(err, data, res){	
 		// POST variables
 		var gifs = req.body.gifs;
 		var nsfw = req.body.nsfw;
@@ -30,29 +36,36 @@ router.use(function (req, res, next) {
 		} else {
  			// Get image links
  			links = get_image_links(data, gifs, nsfw);
- 	    	next();
+ 			next();
  		}
-	}); 
+ 	}); 
 });
 
 /* POST subreddit listing. */
 router.post('/', function(req, res) {
-   res.render('subreddit', { subreddit: req.body.subreddit, images: links, from: req.body.from, sort: req.body.sort});
-   links = [];
+	res.render('subreddit', { 
+		subreddit: req.body.subreddit,
+		images: links, 
+		from: req.body.from, 
+		sort: req.body.sort
+	});
+
+	links = [];
 });
 
 // TODO : Handle imgur albums
+// Gets images from appropriate links in the passed through reddit data
 function get_image_links(reddit_data, gifs, nsfw){
 	var links = [];
 	var children_data = reddit_data['data']['children'];
 
-	for (i = 1; i < children_data.length; ++i){
-		var link = children_data[i]['data']['url'];
-		var commentLink = "http://www.reddit.com" + children_data[i]['data']['permalink'];
+	for (reddit_post = 1; reddit_post < children_data.length; ++reddit_post){
+		var link = children_data[reddit_post]['data']['url'];
+		var commentLink = "http://www.reddit.com" + children_data[reddit_post]['data']['permalink'];
 
 		// Check NSFW
-		if (nsfw != 'on' && children_data[i]['data']['over_18'] != false){
-				continue;			
+		if (nsfw != 'on' && children_data[reddit_post]['data']['over_18'] != false){
+			continue;			
 		}
 
 		// Check for imgur link
@@ -68,7 +81,7 @@ function get_image_links(reddit_data, gifs, nsfw){
 	return links;
 }
 
-
+// Check if the passed in URL is an image 
 function is_image(url, gifs){
 	if (gifs !== undefined){
 		return is_jpg(url) || is_gif(url);
@@ -78,12 +91,14 @@ function is_image(url, gifs){
 	}
 }
 
+// Check if the passed URL is a jpeg image
 function is_jpg(url){
 	if (url.indexOf('.jpg') > -1)
 		return true;
 	return false;
 }
 
+// Check if the passed URL is a gif image
 function is_gif(url){
 	if (url.indexOf('.gif') > -1)
 		return true;
